865a8c8ff9ddc3b8c1e5e9bf577fa721af0bb7c1
SocialLevi <147379433+SocialLevi@users.noreply.github.com>
2025-07-16 06:19:50 +0300

    Update send-otp.js

--- api/send-otp.js
+++ api/send-otp.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf');
 
 // This function would use the Telegram Bot Token from Vercel's Environment Variables
-// const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
